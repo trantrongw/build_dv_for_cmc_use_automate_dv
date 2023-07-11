b07792992b9e92d49cd4e24fb0eec49fa4541f93
NghiaTT <trantrongnghia1991@gmail.com>
2023-07-11 10:43:58 +0700

    done

---
+++ models/CMC/dv/50_satellite/dv__s_l_orderdetail.sql
@@ -0,0 +1,19 @@
+{%- set yaml_metadata -%}
+source_model: "hash__cmis_dbo_orderdetails"
+src_pk: 
+  - "dv_hash_key_l_orderdetail"
+src_payload:
+  - "[TotalBeforeVAT]"
+  - "[IsMain]"
+src_hashdiff: "dv_hashdiff_s_l_orderdetail"
+src_ldts: "dv_load_timestamp"
+src_source: "dv_recordsource"
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+{{ automate_dv.sat(src_pk=metadata_dict["src_pk"],
+                      src_payload=metadata_dict["src_payload"],
+                      src_hashdiff=metadata_dict["src_hashdiff"],
+                      src_ldts=metadata_dict["src_ldts"],
+                      src_source=metadata_dict["src_source"],
+                      source_model=metadata_dict["source_model"]) }}
